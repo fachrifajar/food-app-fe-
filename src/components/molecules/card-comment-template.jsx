8aa79defb5c04900db8d2fe963e05b38e9ec4cea
master <fachri.fajar@gmail.com>
2023-06-06 22:38:59 +0700

    [add] remove duplicates

--- src/components/molecules/card-comment-template.jsx
+++ src/components/molecules/card-comment-template.jsx
@@ -24,8 +24,7 @@ import ButtonTemplate from "../atoms/button-template";
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
-  padding: theme.spacing(1),
-  // textAlign: "center",
+  // padding: theme.spacing(1),
   color: theme.palette.text.secondary,
   padding: "1% 2%",
 }));
--- src/components/molecules/search-result-template.jsx
+++ src/components/molecules/search-result-template.jsx
@@ -9,9 +9,10 @@ import { InputAdornment } from "@mui/material";
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  // padding: theme.spacing(1),
   textAlign: "center",
   color: theme.palette.text.secondary,
+  padding: "1% 2%",
 }));
 
 export default function BasicStack({ result, onClick }) {
